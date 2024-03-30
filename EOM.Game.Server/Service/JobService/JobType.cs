19f10a2ac3a1d7e01ee76acbddd25fe76f3eccf5
Lestat <cameronav23@gmail.com>
2024-03-29 21:20:43 -0500

    Add bard

--- EOM.Game.Server/Service/JobService/JobType.cs
+++ EOM.Game.Server/Service/JobService/JobType.cs
@@ -8,57 +8,30 @@ namespace EOM.Game.Server.Service.JobService
     // New additions or changes to this file should also be made to the 2DA.
     public enum JobType
     {
-        [Job(JobCategoryType.Invalid,
-            "Invalid",
+        [Job("Invalid",
             0,
-            false,
-            "Unused in-game.",
-            false,
-            false)]
+            "Unused in-game.")]
         Invalid = 0,
-        [Job(JobCategoryType.Combat,
-            "Bard",
-            0,
-            false,
-            "Unused in-game.",
-            false,
-            false)]
+        [Job("Bard",
+            5,
+            "Boi that sings.")]
         Bard = 1,
     }
 
     public class JobAttribute : Attribute
     {
-        public JobCategoryType Category { get; set; }
         public string Name { get; set; }
         public int MaxRank { get; set; }
-        public bool IsActive { get; set; }
         public string Description { get; set; }
-        public bool ContributesToJobCap { get; set; }
-        public bool IsShownInCraftMenu { get; set; }
-        public CharacterType CharacterTypeRestriction { get; set; }
-
-        public CombatPointCategoryType CombatPointCategory { get; set; } 
 
         public JobAttribute(
-            JobCategoryType category,
             string name,
             int maxRank,
-            bool isActive,
-            string description,
-            bool contributesToJobCap,
-            bool isShownInCraftMenu,
-            CombatPointCategoryType combatPointCategory = CombatPointCategoryType.Exempt,
-            CharacterType characterTypeRestriction = CharacterType.Invalid)
+            string description)
         {
-            Category = category;
             Name = name;
             MaxRank = maxRank;
-            IsActive = isActive;
             Description = description;
-            ContributesToJobCap = contributesToJobCap;
-            IsShownInCraftMenu = isShownInCraftMenu;
-            CharacterTypeRestriction = characterTypeRestriction;
-            CombatPointCategory = combatPointCategory;
         }
     }
 }
